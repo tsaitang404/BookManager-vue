1d5010f71542260945581cb916f63bd3f810b6cb
tsaitang404 <tsaitang404@gmail.com>
2024-06-16 16:36:47 +0800

    完成 deluser 功能

--- src/components/admin/user/banUser.vue
+++ src/components/admin/user/banUser.vue
@@ -1,11 +1,40 @@
 <template>
-    <select-user/>
+    <div>
+        <select-user v-model:user="selectedUser" />
+        <button @click="confirmDelete">删除用户</button>
+        <Dialog v-model:dialogVisible="dialogVisible" @confirm="deleteUser" @cancel="closeDialog" />
+    </div>
 </template>
+
 <script>
-import selectUser from "@/components/UI/userSelector.vue";
-export default{
-    components:{
-        selectUser
+import SelectUser from "@/components/UI/userSelector.vue";
+import dialog from "@/components/UI/dialog.vue";
+
+export default {
+    components: {
+        SelectUser,
+        dialog
+    },
+    data() {
+        return {
+            selectedUser: null,
+            dialogVisible: false
+        };
+    },
+    methods: {
+        confirmDelete() {
+            if (this.selectedUser) {
+                this.dialogVisible = true;
+            } 
+        },
+        deleteUser() {
+                alert("Delete");
+            // ...
+            this.dialogVisible = false; 
+        },
+        closeDialog() {
+            this.dialogVisible = false; 
+        }
     }
-}
+};
 </script>
